7f74b8e3d8b04f5a1e3da34764290740db0bdb25
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-25 18:56:19 +0545

    -added loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:world_time/services/world_time.dart";
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
@@ -29,10 +29,13 @@ String time='loading';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-          padding:EdgeInsets.all(50.0),
-      child: Text('loading page'),
-      ),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child:SpinKitFadingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      )
     );
   }
 }
